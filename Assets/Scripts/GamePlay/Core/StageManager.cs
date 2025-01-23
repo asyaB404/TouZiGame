65f9ccfe14c0af2c9a75749cd829674ac137300f
kexin233 <148410980+kexin233@users.noreply.github.com>
2025-01-23 22:01:54 +0800

    yapi

--- Assets/Scripts/GamePlay/Core/StageManager.cs
+++ Assets/Scripts/GamePlay/Core/StageManager.cs
@@ -16,9 +16,9 @@ namespace GamePlay.Core
         private static StageManager instance;
 
         //TODO开放性有待商榷
-        public int handNub { get; private set; } //Hand 主要用于扑克等卡牌游戏，表示从发牌到结束的一局，即游戏中完成一次一方获胜而筹码增加的过程；此处代表经过了几次“hand”
-        public int round { get; private set; } //回合数（敌方放一次我放一次为一个回合
-        public int stage { get; private set; } //阶段数（决定是否加注的时候为一个阶段
+        public int handNub { get; private set; }=0; //Hand 主要用于扑克等卡牌游戏，表示从发牌到结束的一局，即游戏中完成一次一方获胜而筹码增加的过程；此处代表经过了几次“hand”
+        public int round { get; private set; }=0; //回合数（敌方放一次我放一次为一个回合
+        public int stage { get; private set; }=0; //阶段数（决定是否加注的时候为一个阶段
         public int firstPlayerId=-1; //这个hand内先手玩家的id
 
         public void NewHand()
@@ -58,6 +58,7 @@ namespace GamePlay.Core
 
         public void SetText()
         {
+            Debug.Log($"handNub:{handNub},round:{round},stage:{stage}");
             GameManager.Instance.SetStage(handNub: handNub + 1, round: round + 1, stage: stage + 1);
         }
     }
