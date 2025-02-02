8e9418567b0edf51ae8e38faf572aa4de5ab463a
bbbasya <42473270+asyaB404@users.noreply.github.com>
2025-02-03 00:23:48 +0800

    settingpanel

---
+++ Assets/Scripts/UI/Panel/SettingsPanel.cs
@@ -0,0 +1,15 @@
+// // ********************************************************************************************
+// //     /\_/\                           @file       SettingsPanel.cs
+// //    ( o.o )                          @brief     Game07
+// //     > ^ <                           @author     Basya
+// //    /     \
+// //   (       )                         @Modified   2025020300
+// //   (___)___)                         @Copyright  Copyright (c) 2025, Basya
+// // ********************************************************************************************
+
+namespace UI.Panel
+{
+    public class SettingsPanel : BasePanel<SettingsPanel>
+    {
+    }
+}
