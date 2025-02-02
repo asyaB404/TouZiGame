1d9b8f3681194be108c50388dda52630258d19c3
kexin233 <148410980+kexin233@users.noreply.github.com>
2025-02-02 22:26:44 +0800

    自己都不知道是什么

--- Assets/Scripts/GamePlay/Core/GameManager.OnlineGame.cs
+++ Assets/Scripts/GamePlay/Core/GameManager.OnlineGame.cs
@@ -22,8 +22,12 @@ namespace GamePlay.Core
             GameMode = GameMode.Online;
             gameObject.SetActive(true);
             GameUIPanel.Instance.ShowMe();
+            SetLayout();
         }
+        private void SetLayout()
+        {
 
+        }
         /// <summary>
         /// 房主开始游戏时所有客户端调用
         /// </summary>
@@ -34,6 +38,8 @@ namespace GamePlay.Core
             holeCardManagers[0].ResetAllHoleCards();
             holeCardManagers[1].gameObject.SetActive(false);
             _jackpotManager.EnterRaise();
+            holeCardManagers[0].HideShader();
+            holeCardManagers[1].HideShader();
             GameUIPanel.Instance.UpdateOnlineUI();
         }
     }
